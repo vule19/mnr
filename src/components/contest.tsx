9646000a1477682b49b646f9dbbde70ff6012773
vule19 <anhvle1901@gmail.com>
2025-01-09 22:41:57 -0500

    5.1

--- src/components/contest.tsx
+++ src/components/contest.tsx
@@ -29,7 +29,7 @@ const Contest = ({ initialContest, onContestListClick }) => {
 
                 <div className="title">Proposed Names</div>
                 <div className="body">
-                    {contest.names?length > 0 ? (
+                    {contest.names?.length > 0 ? (
                         <div className="list">
                             {contest.names.map((proposedName) => (
                                 <div key={proposedName.id} className="item">{proposedName.name}</div>
